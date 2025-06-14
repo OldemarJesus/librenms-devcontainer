1c88594b4bed66f3c67beb724e3bc35dd05cce39
Oldemar Jesus <oldemego@gmail.com>
2025-06-14 23:27:26 +0100

    configured librenms devcontainer

---
+++ .devcontainer/scripts/config.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd /workspaces/librenms-devcontainer/librenms \
+&& source /opt/python/bin/activate \
+&& ./scripts/composer_wrapper.php install --no-dev \
+&& sed -i 's/#DB_HOST=/DB_HOST=db/g' .env \
+&& sed -i 's/#DB_DATABASE=/DB_DATABASE=librenms/g' .env \
+&& sed -i 's/#DB_USERNAME=/DB_USERNAME=librenms/g' .env \
+&& sed -i 's/#DB_PASSWORD=/DB_PASSWORD=mysecretpassword/g' .env \
+&& ./lnms migrate --seed --no-interaction --force
+
+exit 0
