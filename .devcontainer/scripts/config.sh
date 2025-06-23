234bba55d694eb534003a88c4f1b31e45119b7fb
Oldemar Jesus <oldemego@gmail.com>
2025-06-23 22:25:28 +0100

    installed gpg and improve kafka requirements intallation

--- .devcontainer/scripts/config.sh
+++ .devcontainer/scripts/config.sh
@@ -8,7 +8,7 @@ cd /workspaces/librenms-devcontainer/librenms \
 && sed -i 's/#DB_USERNAME=/DB_USERNAME=librenms/g' .env \
 && sed -i 's/#DB_PASSWORD=/DB_PASSWORD=mysecretpassword/g' .env \
 && ./lnms migrate --seed --no-interaction --force \
-&& ./lnms plugin:add idealo/php-rdkafka-ffi \
-&& ./lnms plugin:add ext-ffi
+&& ./scripts/composer_wrapper.php require idealo/php-rdkafka-ffi \
+&& ./scripts/composer_wrapper.php require ext-ffi
 
 exit 0
